3daeac8829e4b2ddc46f10300b3814e6cb8996c6
np8bx@virginia.edu <np8bx@virginia.edu>
2015-09-05 02:28:43 -0400

    initial set up for Xcode and Parse

--- HiiFive/ViewController.swift
+++ HiiFive/ViewController.swift
@@ -6,13 +6,21 @@
 //  Copyright © 2015 NKCH. All rights reserved.
 //
 
+import Parse
 import UIKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
     }
 
     override func didReceiveMemoryWarning() {
